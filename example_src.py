6d7d178f5bcd51b54d5e78af114c01f50bd63023
Adriano Hernandez <adrianoh@mit.edul>
2021-08-22 06:08:37 -0700

    RANGE WORKS POGGERS

--- example_src.py
+++ example_src.py
@@ -1,3 +1,4 @@
+#!callname:fib
 # hello!
 def fib(n):
     if n == 1 or n == 2:
@@ -6,5 +7,8 @@ def fib(n):
         return fib(n-1) + fib(n-2)
 
 # expect 2
-def main():
-    return fib(3)
+def main(n=10, m=0):
+    if m == 0:
+        return fib(n)
+    else:
+        return fib(n) + m*2
