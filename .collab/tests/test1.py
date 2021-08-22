1f129b57c74e8124be19bf101194aecf1cd68ab4
sarbor <sarboroy99@gmail.com>
2021-08-22 05:15:26 -0700

    updated cli to serialize

--- .collab/tests/test1.py
+++ .collab/tests/test1.py
@@ -7,9 +7,9 @@ class Testing(TestExcel):
 	 	result = func(2, 2)
 	 	self.assertEqual(result, 4)
 
-	 def test_data(self):
-	 	formula = '=SUM(A1,B1)'
-	 	path = self.files['uscities.xlsx']
-	 	result = create_unittest_from_file(formula, path)
-	 	self.assertEqual(result, 4)
+	 # def test_data(self):
+	 # 	formula = '=SUM(A1,B1)'
+	 # 	path = self.files['uscities.xlsx']
+	 # 	result = create_unittest_from_file(formula, path)
+	 # 	self.assertEqual(result, 4)
 
